240fca904b97bda4cf748bcec5d12c16a78228ca
Jacob Morehouse <jacob.morehouse@gmail.com>
2017-01-03 21:04:13 -0800

    Add project files.

---
+++ tsaGetWaitTimes/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace tsaGetWaitTimes
+{
+    class databaseOps
+    {
+        private static string connectionString = ConfigurationManager.ConnectionStrings["production"].ConnectionString;
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var theUri = "http://apps.tsa.dhs.gov/MyTSAWebService/GetTSOWaitTimes.ashx?ap="; //needs the airport code after the =, such as SEA.
+            XmlDocument xmlResult = new XmlDocument();
+
+            try
+            {
+                HttpWebRequest webReq = WebRequest.CreateHttp(theUri);
+                webReq.CookieContainer = new CookieContainer();
+                webReq.Method = "GET";
+                webReq.UserAgent = "Something";
+                webReq.Referer = "https://www.tsa.gov";
+
+                WebResponse thisthis = webReq.GetResponse();
+                using (var streamReader = new StreamReader(thisthis.GetResponseStream()))
+                {
+                    var result = streamReader.ReadToEnd();
+                    xmlResult.LoadXml(result);
+                    xmlResult.Save("c:\\users\\jmwin7\\Desktop\\tsaWaitTimesResult.txt"); //working!
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("catch1 triggered: {0}", e.Message);
+            }
+
+        }
+    }
+}
